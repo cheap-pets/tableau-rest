e3c2295b681b23d0b5ffe2714002884cfe12d3bb
scrollbar-ww <scrollbar@163.com>
2018-11-14 12:41:34 +0800

    add 'name' filter @queryView

--- src/api/v3/view.js
+++ src/api/v3/view.js
@@ -2,11 +2,12 @@ const isInteger = require('lodash.isinteger')
 const request = require('request-promise-native')
 
 function queryViews (options = {}) {
-  const { pageSize, pageNumber, projectId, projectName, workbookId, workbookName } = options
+  const { pageSize, pageNumber, name, projectId, projectName, workbookId, workbookName } = options
 
   let query = ''
   if (isInteger(pageSize)) query += `&pageSize=${pageSize}`
   if (isInteger(pageNumber)) query += `&pageNumber=${pageNumber}`
+  if (name) query += `&filter=name:eq:${name}`
 
   let url = workbookId ? `workbooks/${workbookId}/views` : 'views'
   if (query.length) url += '?' + query.substr(1)
@@ -29,8 +30,8 @@ function queryViews (options = {}) {
 }
 
 function getTrustedViewUrl ({ host, site, contentUrl, userName }) {
-  const form = { username: userName }
-  site = site || this.$site
+  const form = { username: userName || this.$options.user }
+  site = (site || site === '') ? site : this.$site
   if (site) form.target_site = site
   request({
     followAllRedirects: true,
@@ -41,6 +42,7 @@ function getTrustedViewUrl ({ host, site, contentUrl, userName }) {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     }
   }).then(ticket => {
+    if (ticket.toString() === '-1') return -1
     const arr = contentUrl.split('/')
     const idx = arr.lastIndexOf('sheets')
     if (idx) arr.splice(idx, 1)
